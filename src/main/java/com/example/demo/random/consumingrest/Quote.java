d2e842d08e605a7ca8b17d2d75c62c912c3f2a70
paddy <pdd337816464@163.com>
2023-04-10 23:22:08 +0800

    删除 - Quote.java

--- src/main/java/com/example/demo/random/consumingrest/Quote.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.random.consumingrest;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.beans.factory.annotation.Value;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Quote(String type, Value value) { }
